8989cdd503eba44367b169468be6243096b586e1
Molaryy <mohammedjbilou768@gmail.com>
2024-08-12 16:29:32 +0200

    feat: projects

--- frontend/src/components/Projects/Projects.tsx
+++ frontend/src/components/Projects/Projects.tsx
@@ -1,7 +1,37 @@
+import projects from "../../test.data.ts";
+import './Projects.css'
+import giftBox from "/gift-box.png"
+
 const Projects = () => {
+
+    const limitSizeDescription = (description: string): string => {
+        if (description.length <= 300) {
+            return description; // No need to truncate if the description is already short enough
+        }
+
+        const lastSpaceIndex = description.lastIndexOf(' ', 300);
+
+        if (lastSpaceIndex > -1) {
+            return description.substring(0, lastSpaceIndex) + "...";
+        }
+
+        return description.substring(0, 300) + "...";
+    };
+
+
     return (
-        <>
-        </>
+        <div className={"projects"}>
+            {projects.map((item) => (
+                <div className={"project-container"}>
+                    <img src={item.backgroundImg} alt={"project-background-image"}/>
+                    <div className={"project-text-container"}>
+                        <h3 id={"project-name"}>{item.name}</h3>
+                        <p id={"project-description"}>{ limitSizeDescription(item.description)}</p>
+                    </div>
+                    <button id={"project-button-donate"}>Donate <img id={"gift-box-button"} src={giftBox} alt={"gift-box"}/> </button>
+                </div>
+            ))}
+        </div>
     )
 }
 
