affcd88a5f1a7f0e51b450a9154cba56834433d8
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-08-21 00:47:32 +0200

    feat: Connect wallet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,31 +3,30 @@ import Navbar from "./components/Navbar/Navbar.tsx";
 import Footer from "./components/Footer/Footer.tsx";
 import Projects from "./components/Projects/Projects.tsx";
 import Numbers from "./components/Numbers/Numbers.tsx";
-import {
-    BrowserRouter,
-    Route,
-    Routes
-} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {ChakraProvider} from "@chakra-ui/react";
 
 function App() {
 
     return (
         <div className={"app"}>
-            <Navbar/>
-            <BrowserRouter>
-                <Routes>
-                    <Route path={"/"} element={
-                        <div>
-                            <Numbers/>
-                            <Projects/>
-                        </div>
-                    }/>
-                    <Route path={"/home"} element={<h1>Hello</h1>}/>
-                </Routes>
-            </BrowserRouter>
-            <Footer/>
+            <ChakraProvider>
+                <Navbar/>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path={"/"} element={
+                            <div>
+                                <Numbers/>
+                                <Projects/>
+                            </div>
+                        }/>
+                        <Route path={"/home"} element={<h1>Hello</h1>}/>
+                    </Routes>
+                </BrowserRouter>
+                <Footer/>
+            </ChakraProvider>
         </div>
-  )
+    )
 }
 
 export default App;
---
+++ frontend/src/stores/account.ts
@@ -0,0 +1,23 @@
+import {create} from "zustand";
+import {User} from "../types/user.ts";
+
+type AccountUser = Omit<User, "address">;
+
+type AccountState = {
+    address: string | null;
+    user: AccountUser | null;
+};
+
+type AccountActions = {
+    setAddress: (address: string | null) => void;
+    setUser: (user: AccountUser | null) => void;
+};
+
+const useAccountStore = create<AccountState & AccountActions>((set) => ({
+    address: null,
+    user: null,
+    setAddress: (address) => set(() => ({address})),
+    setUser: (user) => set(() => ({user})),
+}));
+
+export {useAccountStore};
